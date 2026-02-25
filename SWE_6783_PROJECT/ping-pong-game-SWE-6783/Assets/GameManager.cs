2bdd7fe214871a9fcf3d474fd5191529976cc609
waustin45 <108015666+waustin45@users.noreply.github.com>
2026-02-24 21:02:29 -0500

    fixed ball bounce and speed

--- SWE_6783_PROJECT/ping-pong-game-SWE-6783/Assets/GameManager.cs
+++ SWE_6783_PROJECT/ping-pong-game-SWE-6783/Assets/GameManager.cs
@@ -30,23 +30,23 @@ public class GameManager : MonoBehaviour {
 	}
 
 	void OnGUI() {
-		GUI.skin = layout;
-		GUI.Label (new Rect (Screen.width / 2 - 150 - 12, 20, 100, 100), "" + PlayerScore1);
-		GUI.Label (new Rect (Screen.width / 2 + 150 + 12, 20, 100, 100), "" + PlayerScore2);
+		// GUI.skin = layout;
+		// GUI.Label (new Rect (Screen.width / 2 - 150 - 12, 20, 100, 100), "" + PlayerScore1);
+		// GUI.Label (new Rect (Screen.width / 2 + 150 + 12, 20, 100, 100), "" + PlayerScore2);
 
-		if (GUI.Button (new Rect (Screen.width / 2 - 60, 35, 120, 53), "RESTART")) {
-			PlayerScore1 = 0;
-			PlayerScore2 = 0;
-			theBall.SendMessage ("RestartGame", 0.5f, SendMessageOptions.RequireReceiver);
-		}
+		// if (GUI.Button (new Rect (Screen.width / 2 - 60, 35, 120, 53), "RESTART")) {
+		// 	PlayerScore1 = 0;
+		// 	PlayerScore2 = 0;
+		// 	theBall.SendMessage ("RestartGame", 0.5f, SendMessageOptions.RequireReceiver);
+		// }
 
-		if (PlayerScore1 == 3) {
-			GUI.Label (new Rect (Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER ONE WINS");
-			theBall.SendMessage ("ResetBall", null, SendMessageOptions.RequireReceiver);
-		} else if (PlayerScore2 == 3) {
-			GUI.Label (new Rect (Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER TWO WINS");
-			theBall.SendMessage ("ResetBall", null, SendMessageOptions.RequireReceiver);
-		}
+		// if (PlayerScore1 == 3) {
+		// 	GUI.Label (new Rect (Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER ONE WINS");
+		// 	theBall.SendMessage ("ResetBall", null, SendMessageOptions.RequireReceiver);
+		// } else if (PlayerScore2 == 3) {
+		// 	GUI.Label (new Rect (Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER TWO WINS");
+		// 	theBall.SendMessage ("ResetBall", null, SendMessageOptions.RequireReceiver);
+		// }
 	}
 
 }
