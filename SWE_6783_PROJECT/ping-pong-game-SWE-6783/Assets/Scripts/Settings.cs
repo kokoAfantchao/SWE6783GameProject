892957bb8ff6bd4e369afeab45d277edaaff6baf
Ysherif1 <20ysherif01@gmail.com>
2026-03-08 19:51:55 -0400

    Added settings page with sound toggle and difficulty levels

---
+++ SWE_6783_PROJECT/ping-pong-game-SWE-6783/Assets/Scripts/Settings.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Settings : MonoBehaviour
+{
+    public Toggle soundToggle;
+    public Dropdown difficultyDropdown;
+
+    void Start()
+    {
+        soundToggle.isOn = PlayerPrefs.GetInt("Sound", 1) == 1;
+        difficultyDropdown.value = PlayerPrefs.GetInt("Difficulty", 0);
+
+        ApplySound();
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetInt("Sound", soundToggle.isOn ? 1 : 0);
+        PlayerPrefs.SetInt("Difficulty", difficultyDropdown.value);
+
+        PlayerPrefs.Save();
+        ApplySound();
+    }
+
+    void ApplySound()
+    {
+        AudioListener.pause = !soundToggle.isOn;
+    }
+
+    public void BackToMenu()
+    {
+        SaveSettings();
+        SceneManager.LoadScene("MainMenu");
+    }
+}
