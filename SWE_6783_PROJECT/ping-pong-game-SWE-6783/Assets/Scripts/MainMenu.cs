6e889fc8e7024a28295294f2c094bbebad504367
waustin45 <108015666+waustin45@users.noreply.github.com>
2026-03-13 08:05:26 -0400

    added three levels

--- SWE_6783_PROJECT/ping-pong-game-SWE-6783/Assets/Scripts/MainMenu.cs
+++ SWE_6783_PROJECT/ping-pong-game-SWE-6783/Assets/Scripts/MainMenu.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame(){
-        SceneManager.LoadScene("Scene");
+        SceneManager.LoadScene("Level_One");
     }
 
     public void QuitGame(){
